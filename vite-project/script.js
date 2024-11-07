5d17d2b9f08b431962e9b0fa9f4ced5251f046e1
Luke <94772285+ACoderGuy1002@users.noreply.github.com>
2024-11-07 10:03:34 -0600

    Pleasecloudflare

--- vite-project/script.js
+++ vite-project/script.js
@@ -1,7 +1,15 @@
-export function goto(){
-    let inputTextValue = document.getElementById("url")
+
+
+function goto(){
+    console.log("HELLO");
+
+    let inputTextValue = document.getElementById("url");
 
     inputTextValue = inputTextValue.value;
 
-    window.open('https://www.duckduckgo.com/?t=lm&q=' + inputTextValue).focus;
+    window.open('https://www.duckduckgo.com/?t=h_&q=' + inputTextValue).focus;
 }
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", goto);
