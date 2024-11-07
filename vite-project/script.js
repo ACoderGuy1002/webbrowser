4ed6df0d232e03e417335090390779c5f1c31920
Luke <94772285+ACoderGuy1002@users.noreply.github.com>
2024-11-07 11:55:57 -0600

    Letshopework

--- vite-project/script.js
+++ vite-project/script.js
@@ -6,10 +6,25 @@ function goto(){
     let inputTextValue = document.getElementById("url");
 
     inputTextValue = inputTextValue.value;
+    var strUsr = drop.options[drop.selectedIndex].value;
 
-    window.open('https://www.duckduckgo.com/?t=h_&q=' + inputTextValue).focus;
+    if (strUsr == "duck"){
+        window.open('https://www.duckduckgo.com/?t=h_&q=' + inputTextValue).focus;
+    }
+    else if (strUsr == "google"){
+        window.open('https://www.google.com/search?q=' + inputTextValue).focus;
+    }
+    else {
+        window.open('https://www.bing.com/search?q=' + inputTextValue).focus;
+    }
+
 }
 
 const form = document.getElementById("form");
+const drop = document.getElementById("engine")
 
 form.addEventListener("submit", goto);
+
+function changesearch(){
+
+}
